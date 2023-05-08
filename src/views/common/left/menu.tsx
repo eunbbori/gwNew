a9bd2dfbad2d5aabfb77aec0c03a085c1c101be5
eunbbori <eunbbori@gmail.com>
2023-05-08 15:09:07 +0900

    Menu 컴포넌트 분리 후 selectedMenu props로 전달

---
+++ src/views/common/left/menu.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+// import MenuItem from './menuItem';
+
+import laptopIcon from 'src/assets/svg/laptop.svg';
+import userGroupIcon from 'src/assets/svg/user-group.svg';
+import flightIcon from 'src/assets/svg/flight.svg';
+import alarmIcon from 'src/assets/svg/alarm.svg';
+import editIcon from 'src/assets/svg/edit.svg';
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
+
+const MenuItem = dynamic(import('../left/menuItem'));
+export interface IMenuProps {
+  selectedMenu: string;
+}
+const Menu = ({ selectedMenu }: IMenuProps) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <aside className="max-w-62.5 ease-nav-brand z-990 fixed my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
+      <div className="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+        <ul className="flex flex-col pl-0 mb-0">
+          <Link href="/dashboard">
+            <MenuItem menuNm="대시보드" imgSrc={laptopIcon} selected={'dashboard' !== selectedMenu} />
+          </Link>
+          <Link href="/member">
+            <MenuItem menuNm="직원관리" imgSrc={userGroupIcon} selected={'member' !== selectedMenu} />
+          </Link>
+          <Link href="/dayoff">
+            <MenuItem menuNm="휴가관리" imgSrc={flightIcon} selected={'dayoff' !== selectedMenu} />
+          </Link>
+          <Link href="/attendance">
+            <MenuItem menuNm="근태관리" imgSrc={alarmIcon} selected={'attendance' !== selectedMenu} />
+          </Link>
+          <Link href="/approval">
+            <MenuItem menuNm="결제" imgSrc={editIcon} selected={'approval' !== selectedMenu} />
+          </Link>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Menu;
--- src/views/common/left/menuItem.tsx
+++ src/views/common/left/menuItem.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image';
 export interface IMenuItemProps {
   menuNm: string;
   imgSrc: any;
-  url: string;
+  url?: string;
   selected?: boolean;
 }
 
 const MenuItem = ({ menuNm, imgSrc, url, selected }: IMenuItemProps) => {
   const anchorClass = 'py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors ';
   const divClass = 'shadow-soft-2xl mr-2 flex h-10 w-10 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5 ';
-  const anchorSelectedClass = 'shadow-soft-xl ounded-lg bg-white font-semibold text-slate-700';
+  const anchorSelectedClass = 'shadow-soft-xl rounded-lg bg-white font-semibold text-slate-700';
   const divSelectedClass = 'bg-gradient-to-tl from-purple-700 to-pink-500';
 
   return (
     <li className="mt-0.5 w-full">
-      <a className={anchorClass + (selected ? anchorSelectedClass : '')} href={url}>
+      <a className={anchorClass + (selected ? anchorSelectedClass : '')}>
         <div className={divClass + (selected ? divSelectedClass : '')}>
           <Image src={imgSrc} alt={menuNm} />
         </div>
