270b1b58997a1acd6a0b8d96ed1ff18977d257d2
eunbbori <eunbbori@gmail.com>
2023-05-09 16:43:15 +0900

    oldboy - 출퇴근시간 형식 수정

--- src/views/attendance/tableRows.tsx
+++ src/views/attendance/tableRows.tsx
@@ -6,24 +6,43 @@ type TableRowsProps = {
 };
 
 const TableRows = ({ data }: TableRowsProps) => {
-  console.log('data', data);
-  console.log('data.employees', data.employees);
-  console.log('data.employees[0]', data?.employees![0]);
-  console.log('data.employees[0]', data?.employees![0]!['employeeName']);
-
   return (
     <>
-      {data.employees!.map((e, id) => (
-        <tr key={id}>
-          <TableCell cellData={e!['employeeName']} />
-          <TableCell cellData={''} />
-          <TableCell cellData={'출근'} />
-          <TableCell cellData={e!['startAt']} />
-          <TableCell cellData={e!['endAt']} />
-          <TableCell cellData={'-'} />
-          <TableCell cellData={'-'} />
-        </tr>
-      ))}
+      {data.employees!.map((e, id) => {
+        const startAt: Date = e && e.startAt && new Date(e.startAt);
+        const endAt: Date = e && e.endAt && new Date(e.endAt);
+
+        const startTm =
+          startAt &&
+          startAt.getMonth() +
+            '월' +
+            startAt.getDate() +
+            '일 ' +
+            startAt.getHours().toString().padStart(2, '0') +
+            ':' +
+            startAt.getMinutes().toString().padStart(2, '0');
+        const endTm =
+          endAt &&
+          endAt.getMonth() +
+            '월' +
+            endAt.getDate() +
+            '일 ' +
+            endAt.getHours().toString().padStart(2, '0') +
+            ':' +
+            endAt.getMinutes().toString().padStart(2, '0');
+
+        return (
+          <tr key={id}>
+            <TableCell cellData={e!['employeeName']} />
+            <TableCell cellData={''} />
+            <TableCell cellData={'출근'} />
+            <TableCell cellData={startTm} />
+            <TableCell cellData={endTm} />
+            <TableCell cellData={'-'} />
+            <TableCell cellData={'-'} />
+          </tr>
+        );
+      })}
     </>
   );
 };
