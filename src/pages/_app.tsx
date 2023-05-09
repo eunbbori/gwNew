c788e907f309293a810c2e647d233bbe08967a42
eunbbori <eunbbori@gmail.com>
2023-05-09 15:31:10 +0900

    데이터 가져오기 test  (data console.log만 성공)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
--- src/types/generated/types.ts
+++ src/types/generated/types.ts
@@ -1,8 +1,11 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -34,3 +37,47 @@ export type IQuery = {
   departments?: Maybe<Array<Maybe<IDepartment>>>;
   employees?: Maybe<Array<Maybe<IEmployee>>>;
 };
+
+export type IGetEmployeeQueryVariables = Exact<{ [key: string]: never }>;
+
+export type IGetEmployeeQuery = {
+  __typename?: 'Query';
+  employees?: Array<{ __typename?: 'Employee'; employeeId?: number | null; userId?: string | null; employeeName?: string | null } | null> | null;
+};
+
+export const GetEmployeeDocument = gql`
+  query getEmployee {
+    employees {
+      employeeId
+      userId
+      employeeName
+    }
+  }
+`;
+
+/**
+ * __useGetEmployeeQuery__
+ *
+ * To run a query within a React component, call `useGetEmployeeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetEmployeeQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetEmployeeQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetEmployeeQuery(baseOptions?: Apollo.QueryHookOptions<IGetEmployeeQuery, IGetEmployeeQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<IGetEmployeeQuery, IGetEmployeeQueryVariables>(GetEmployeeDocument, options);
+}
+export function useGetEmployeeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<IGetEmployeeQuery, IGetEmployeeQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<IGetEmployeeQuery, IGetEmployeeQueryVariables>(GetEmployeeDocument, options);
+}
+export type GetEmployeeQueryHookResult = ReturnType<typeof useGetEmployeeQuery>;
+export type GetEmployeeLazyQueryHookResult = ReturnType<typeof useGetEmployeeLazyQuery>;
+export type GetEmployeeQueryResult = Apollo.QueryResult<IGetEmployeeQuery, IGetEmployeeQueryVariables>;
