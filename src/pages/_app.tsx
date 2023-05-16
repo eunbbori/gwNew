82ec738c2aa0584d5b6d0789aa84b6e7c59f2b93
eunbbori <eunbbori@gmail.com>
2023-05-16 17:14:17 +0900

    nextAuth jwt 임의 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,14 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { SessionProvider } from 'next-auth/react';
 
 const client = new ApolloClient({
+  // uri: 'http://localhost:8080/graphql',
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,31 +4,36 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 export default NextAuth({
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: '이메일,패스워드 방식',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: { label: '이메일', type: 'email', placeholder: 'user@jnfirst.com' },
         password: { label: '비밀번호', type: 'password' },
       },
       async authorize(credentials: any, req) {
-        // Add logic here to look up the user from the credentials supplied
         const user = { id: '1', name: '아이언맨', email: 'ironman@jnfirst.com' };
 
         if (credentials.email === 'ironman@jnfirst.com' && credentials.password === 'test') {
-          // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
-          // If you return null or false then the credentials will be rejected
           return null;
-          // You can also Reject this callback with an Error or with a URL:
-          // throw new Error("error message") // Redirect to error page
-          // throw "/path/to/redirect"        // Redirect to a URL
         }
       },
     }),
   ],
+  // session: {
+  //   strategy: 'jwt',
+  // },
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, token, user }) {
+  //     // Send properties to the client, like an access_token from a provider.
+  //     session.accessToken = token.accessToken;
+  //     return session;
+  //   },
+  // },
 });
