11aa979f08d385f6bd38129516b36876e801605b
eunbbori <eunbbori@gmail.com>
2023-05-16 17:14:39 +0900

    출퇴근 버튼 css 수정

--- src/components/AttendanceBtnGroup.tsx
+++ src/components/AttendanceBtnGroup.tsx
@@ -8,8 +8,8 @@ const AttendanceBtnGroup = () => {
   const [workState, setWorkState] = useState(false);
   const workBtnClass =
     'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700';
-  const workBtnDisabledClass = 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 bg-red-300';
-
+  const workBtnDisabledClass = 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 rounded-lg border border-gray-200 bg-blue-400';
+  const offBtnDisabledClass = 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 rounded-lg border border-gray-200 bg-red-400';
   const workBtnHandler = () => {
     const clickedStartAt = Date();
     setWorkState(true);
@@ -39,12 +39,15 @@ const AttendanceBtnGroup = () => {
           </li>
           <li className="flex items-center">
             <div className="block px-0 py-2 font-semibold transition-all ease-nav-brand text-sm text-slate-500 ml-6">
-              <button
-                className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                onClick={offBtnHandler}
-              >
-                퇴근
-              </button>
+              {workState !== true ? (
+                <button className={`${offBtnDisabledClass}`} disabled onClick={offBtnHandler}>
+                  퇴근
+                </button>
+              ) : (
+                <button className={`${workBtnClass}`} onClick={offBtnHandler}>
+                  퇴근
+                </button>
+              )}
             </div>
           </li>
         </>
