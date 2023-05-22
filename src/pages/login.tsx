5baf5b21f3b86fd9dc2cc7ea66063028b9cef236
eunbbori <eunbbori@gmail.com>
2023-05-22 09:47:41 +0900

    로그인 UI 2)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -32,13 +32,17 @@ const Login: React.FC = () => {
   };
   return (
     <div className="w-full mr-auto ml-auto px-6">
-      <div className="w-full max-w-full px-3 mx-auto mt-0 md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12">
+      <div className="w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12 mt-[150px] mb-[200px]">
         <div className="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
           <div className="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
-            <h5>Login</h5>
+            <div className="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+              <h3 className="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Welcome back</h3>
+              <p className="mb-0">Enter your email and password to sign in</p>
+            </div>
           </div>
           <div className="flex-auto p-6">
             <form onSubmit={handleSubmit(onLogin)}>
+              <label className="mb-2 ml-1 font-bold text-xs text-slate-700">Email</label>
               <div className="mb-4">
                 <input
                   {...register('EmpEmail')}
@@ -48,25 +52,26 @@ const Login: React.FC = () => {
                   aria-label="EmpEmail"
                   aria-describedby="EmpEmail-addon"
                 />
-                <div>{errors.EmpEmail?.message}</div>
+                <div className="text-xs text-red-600">{errors.EmpEmail?.message}</div>
               </div>
+              <label className="mb-2 ml-1 font-bold text-xs text-slate-700">Password</label>
               <div className="mb-4">
                 <input
                   {...register('EmpPassword')}
                   placeholder="비밀번호"
-                  type="test"
+                  type="password"
                   className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
                   aria-label="Password"
                   aria-describedby="Password-login"
                 />
-                <div>{errors.EmpPassword?.message}</div>
+                <div className="text-xs text-red-600">{errors.EmpPassword?.message}</div>
               </div>
               <div className="text-center">
                 <button
                   type="submit"
                   className="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
                 >
-                  로그인
+                  SIGN IN
                 </button>
               </div>
             </form>
