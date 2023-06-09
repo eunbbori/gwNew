3fe5553b044d7fd9f6bb14c99e177cffd1a1ab48
eunbbori <eunbbori@gmail.com>
2023-06-09 10:07:15 +0900

    FIX - gwNew에서만 document is not defined 되는 오류 해결

--- src/pages/attendance/listAttendance.tsx
+++ src/pages/attendance/listAttendance.tsx
@@ -1,10 +1,13 @@
-import Attendance from '@/views/attendance/Attendance';
 import React from 'react';
+import dynamic from 'next/dynamic';
 
 const ListAttendance = () => {
+  const DynamicAttendance = dynamic(() => import('@/views/attendance/Attendance'), {
+    ssr: false,
+  });
   return (
     <div>
-      <Attendance />
+      <DynamicAttendance />
     </div>
   );
 };
