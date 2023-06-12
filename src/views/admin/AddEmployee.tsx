daa8b1663213b4978bff4c05ee33d35d5542aa8a
eunbbori <eunbbori@gmail.com>
2023-06-12 16:50:34 +0900

    FEATURE - 구성원 직원등록 (관리자 기능) UI 생성
    ### DESC
    - 우선 필요한 최소한의 필드 등록
    - 아이디(회사이메일),사원명,부서,계약형태,핸드폰번호,개인이메일,입사일
    - yup 라이브러리를 통해 유효성 검증 (핸드폰번호 형식 000-1111-2222, 입사일 형식 YYYY-MM-DD)
    
    ### TODO
    - 추후에 추가되는 필드에 따라서 UI 변경
    - '부서'와 '계약형태'는 공통코드에서 데이터 fetching

--- src/views/admin/AddEmployee.tsx
+++ src/views/admin/AddEmployee.tsx
@@ -4,19 +4,35 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 export interface EmployeeFormValues {
-  employeeId: number;
   userId: string;
   employeeName: string;
   department: string;
+  contract: string;
+  phone: string;
+  email: string;
+  joinDate: string;
 }
 const AddEmployee: React.FC = () => {
   const schema = yup.object().shape({
-    employeeId: yup.number().typeError('숫자를 입력해주세요').required('사번을 입력해주세요.').positive('양수를 입력해주세요').integer('정수를 입력해주세요'),
-    userId: yup.string().required('아이디를 입력해주세요.'),
-    employeeName: yup.string().required('이름을 입력해주세요.'),
-    department: yup.string().required('부서명을 선택해주세요.'),
+    userId: yup.string().required('아이디는 필수 입력사항입니다.'),
+    employeeName: yup.string().required('이름은 필수 입력사항입니다.'),
+    department: yup.string().required('부서명은 필수 입력사항입니다.'),
+    phone: yup
+      .string()
+      .required('핸드폰 번호는 필수 입력사항입니다.')
+      .matches(/^(?:\+?([0-9]{1,3}))?[- (]*(\d{3,4})[- )]*(\d{3,4})$/, '올바른 핸드폰 번호 형식이 아닙니다.'),
+    email: yup
+      .string()
+      .required('이메일은 필수 입력사항입니다')
+      .matches(/^[^@\s]+@[^@\s]+\.[^@\s]+$/, '이메일 형식에 맞지 않습니다.'),
+    joinDate: yup
+      .string()
+      .required('입사일은 필수 입력사항입니다')
+      .matches(/^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/, '올바른 날짜 형식이 아닙니다.'),
   });
-
+  const inputClassName =
+    'text-[14px] text-[#484848] bg-[#fafafa] focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-[4px] border-2 border-solid border-[#e8e8e8] bg-clip-padding py-2 px-3 font-normal transition-all focus:border-fuchsia-200 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow';
+  const errMsgClassName = 'text-[11px] text-red-400';
   const {
     register,
     handleSubmit,
@@ -26,67 +42,92 @@ const AddEmployee: React.FC = () => {
     resolver: yupResolver(schema),
   });
 
-  const inputStyle =
-    'mb-[10px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
-  const submitBtnStyle =
-    'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800';
-
   const onAddEmployee = (data: EmployeeFormValues) => {
     console.log(data);
   };
   return (
-    <div className="w-full mr-auto ml-auto px-6">
+    <div className="w-full mr-auto ml-auto mt-[25vh] px-6">
       <div className="flex flex-wrap -mx-3 -mt-48 md:-mt-56 lg:-mt-48">
         <div className="w-full max-w-full px-3 mx-auto mt-0 md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12">
-          <div className="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
+          <div className="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 rounded-2xl bg-clip-border">
             <div className="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
-              <h5>Register with</h5>
+              <h5 className="font-bold text-[#484848] text-[20px]">직원 추가</h5>
             </div>
             <div className="flex-auto p-6">
               <form onSubmit={handleSubmit(onAddEmployee)} role="form text-left">
-                <div className="mb-4">
-                  <input
-                    {...register('employeeId')}
-                    placeholder="사번"
-                    type="text"
-                    className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
-                    aria-label="EmpId"
-                    aria-describedby="EmpId-addon"
-                  />
-                  <div>{errors.employeeId?.message}</div>
+                <div className="mb-4 flex justify-between">
+                  <div>
+                    <p className="text-sm text-[#484848] w-[300px]">아이디(회사 이메일)</p>
+                    <input
+                      {...register('userId')}
+                      placeholder="id@jnfirst.co.kr"
+                      type="text"
+                      className={inputClassName}
+                      aria-label="Id"
+                      aria-describedby="Id-addon"
+                    />
+                    <div className={errMsgClassName}>{errors.userId?.message}</div>
+                  </div>
+                  <div>
+                    <p className="text-sm text-[#484848] w-[200px]">이름</p>
+                    <input
+                      {...register('employeeName')}
+                      placeholder="이름을 입력해주세요"
+                      type="text"
+                      className={inputClassName}
+                      aria-label="Name"
+                      aria-describedby="Name-addon"
+                    />
+                    <div className={errMsgClassName}>{errors.employeeName?.message}</div>
+                  </div>
+                </div>
+                <div className="mb-4 flex justify-between">
+                  <div>
+                    <p className="text-sm text-[#484848] w-[300px]">부서</p>
+                    <select className={inputClassName} {...register('department')} placeholder="부서를 선택해주세요">
+                      <option value="0">경영지원부</option>
+                      <option value="1">연구소</option>
+                      <option value="2">개발부</option>
+                    </select>
+                  </div>
+                  <div>
+                    <p className="text-sm text-[#484848] w-[200px]">계약형태</p>
+                    <select className={inputClassName} {...register('contract')} placeholder="계약형태를 선택해주세요">
+                      <option value="0">선택안됨</option>
+                      <option value="1">정규직</option>
+                      <option value="2">프리랜서</option>
+                    </select>
+                  </div>
                 </div>
                 <div className="mb-4">
+                  <p className="text-sm text-[#484848]">핸드폰번호</p>
                   <input
-                    {...register('userId')}
-                    placeholder="아이디"
-                    type="test"
-                    className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
-                    aria-label="Id"
-                    aria-describedby="Id-addon"
-                  />
-                  <div>{errors.userId?.message}</div>
-                </div>
-                <div className="mb-4">
-                  <input
-                    {...register('employeeName')}
-                    placeholder="이름"
-                    type="text"
-                    className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+                    {...register('phone')}
+                    placeholder="핸드폰번호를 입력해주세요"
+                    className={inputClassName}
                     aria-label="Name"
                     aria-describedby="Name-addon"
                   />
-                  <div>{errors.employeeName?.message}</div>
+                  <div className={errMsgClassName}>{errors.phone?.message}</div>
                 </div>
                 <div className="mb-4">
-                  <select
-                    className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
-                    {...register('department')}
-                  >
-                    <option value="0">경영지원부</option>
-                    <option value="1">연구소</option>
-                    <option value="2">개발부</option>
-                  </select>
+                  <p className="text-sm text-[#484848]">이메일(개인)</p>
+                  <input
+                    {...register('email')}
+                    placeholder="개인 이메일을 입력해주세요"
+                    type="email"
+                    className={inputClassName}
+                    aria-label="Name"
+                    aria-describedby="Name-addon"
+                  />
+                  <div className={errMsgClassName}>{errors.email?.message}</div>
                 </div>
+                <div className="mb-4">
+                  <p className="text-sm text-[#484848]">입사일</p>
+                  <input {...register('joinDate')} placeholder="YYYY-MM-DD" className={inputClassName} aria-label="Name" aria-describedby="Name-addon" />
+                  <div className={errMsgClassName}>{errors.joinDate?.message}</div>
+                </div>
+                <div className="mb-4"></div>
                 <div className="text-center">
                   <button
                     type="submit"
@@ -101,26 +142,6 @@ const AddEmployee: React.FC = () => {
         </div>
       </div>
     </div>
-
-    // <div className="flex">
-    //   <form onSubmit={handleSubmit(onAddEmployee)}>
-    //     <div>
-    //       <input className={inputStyle} {...register('employeeId')} placeholder="사번" />
-    //       <div>{errors.employeeId?.message}</div>
-    //       <input className={inputStyle} {...register('userId')} placeholder="아이디" />
-    //       <div>{errors.userId?.message}</div>
-    //       <input className={inputStyle} {...register('employeeName')} placeholder="이름" />
-    //       <div>{errors.employeeName?.message}</div>
-    //       <input className={inputStyle} {...register('departmentName')} placeholder="부서명" />
-    //       <div>{errors.departmentName?.message}</div>
-    //     </div>
-    //     <div className="text-center">
-    //       <button className={submitBtnStyle} type="submit">
-    //         제출
-    //       </button>
-    //     </div>
-    //   </form>
-    // </div>
   );
 };
 
