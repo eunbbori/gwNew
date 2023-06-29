f8ed45b3ea43ffa8314dbc10d8264a9ae71a738a
eunbbori <eunbbori@gmail.com>
2023-06-29 10:46:59 +0900

    FIX

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,7 +76,7 @@ const Header = () => {
               <li className="relative">
                 <a
                   className="hidden-arrow flex items-center whitespace-nowrap transition duration-150 ease-in-out motion-reduce:transition-none"
-                  href="#"
+                  // href="#"
                   type="button"
                   data-te-dropdown-toggle-ref
                   aria-expanded="false"
