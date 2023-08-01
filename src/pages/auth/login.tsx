5657e283eadaf5b8b0488014f6c4098bf4b28bb3
eunbbori <eunbbori@gmail.com>
2023-08-01 11:55:55 +0900

    로그인 시 이메일 형식 입력하도록

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -17,7 +17,7 @@ const Login = () => {
   const loginSchema = yup.object().shape({
     empEmail: yup
       .string()
-      //.matches(/^[^@\s]+@[^@\s]+\.[^@\s]+$/, '이메일 형식에 맞지 않습니다.')
+      .matches(/^[^@\s]+@[^@\s]+\.[^@\s]+$/, '이메일 형식에 맞지 않습니다.')
       .required('이메일을 입력해주세요.'),
     empPassword: yup
       .string()
