976a66ff49cc6524cfc3b0599e73d802c6bf9460
eunbbori <eunbbori@gmail.com>
2023-08-14 17:40:56 +0900

    FEATURE - 직원 목록 프로필 UI 애니메이션 적용

--- src/components/Employee/Profile/Profile.tsx
+++ src/components/Employee/Profile/Profile.tsx
@@ -19,28 +19,30 @@ const Profile = ({ empName, photoUrl, deptName, position, positionOptions, email
   const isAdmin = useUserInfo?.adminYn === 'YES';
 
   return (
-    <div className="mr-[35px] mb-[105px]">
+    <div className="mr-[35px] mb-[10px]">
       <div className="flex">
-        <div className="w-[144px] h-[144px]">
-          <div
-            data-te-target="#detailModal"
-            data-te-ripple-color="light"
-            className="w-[144px] h-[144px] relative rounded-[8px] overflow-hidden"
-            {...(isAdmin && {
-              'data-te-toggle': 'modal',
-              'data-te-ripple-init': '',
-            })}
-          >
-            <Image
-              className={isAdmin ? 'cursor-pointer' : ''}
-              src={photoUrl ? process.env.NEXT_PUBLIC_BASE_PROFILE_API + '/' + photoUrl : blankProfile}
-              alt={empName || ''}
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
-              onClick={onClick}
-            />
+        <div className="w-[144px]">
+          <div className="w-[144px] h-[144px] rounded-[8px] hover:rounded-[20px] ease-in-out duration-300 overflow-hidden">
+            <div
+              data-te-target="#detailModal"
+              data-te-ripple-color="light"
+              className="w-[144px] h-[144px] relative hover:scale-110 ease-in-out duration-300"
+              {...(isAdmin && {
+                'data-te-toggle': 'modal',
+                'data-te-ripple-init': '',
+              })}
+            >
+              <Image
+                className={isAdmin ? 'cursor-pointer' : ''}
+                src={photoUrl ? process.env.NEXT_PUBLIC_BASE_PROFILE_API + '/' + photoUrl : blankProfile}
+                alt={empName || ''}
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: 'auto' }}
+                onClick={onClick}
+              />
+            </div>
           </div>
           <p className="text-[#484848] text-xs mt-[10px] pt-[2px] text-center">{deptName + ' ' + (positionOptions?.get(position || '') ?? '')}</p>
           <p className="text-black font-bold pt-[2px] text-center">{empName}</p>
