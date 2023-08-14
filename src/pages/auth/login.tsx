c89cc5ab8edeeafed3cd40774fb69ee5c438fb26
eunbbori <eunbbori@gmail.com>
2023-08-14 15:41:40 +0900

    FEATURE - 로그인화면 로고(첨부이미지 참고) 및 라이선스 표시
    ### DESC
    - 로그인 및 그외 문구 삭제
    - 메인로고로 대체

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -10,7 +11,7 @@ import Spinner from '@/components/Spinner';
 import { classNames, yupEmail, yupPassword } from '@/views/admin/HandleEmployee';
 import { useUserToken } from '@/repository/AccessToken';
 import { useReactiveVar } from '@apollo/client';
-
+import jnFirstLogo from 'src/assets/img/logos/FAPIG.png';
 export interface ILoginFormValues {
   empEmail: string;
   empPassword: string;
@@ -74,54 +75,61 @@ const Login = () => {
   // if (isLoading) return <Spinner />;
 
   return (
-    <div className="w-full mr-auto ml-auto px-6">
-      <div className="w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12 mt-[0px] mb-[200px] pt-[150px]">
-        <div className="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
-          <div className="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
-            <div className="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
-              <h3 className="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">로그인</h3>
-              <p className="mb-0">이메일과 비밀번호를 입력해주세요.</p>
+    <>
+      <div className="w-full mr-auto ml-auto px-6">
+        <div className="w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12 mt-[0px] mb-[200px] pt-[150px]">
+          <div className="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
+            <div className="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
+              <div className="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+                <Image src={jnFirstLogo} className="inline w-52 h-full" alt="main_logo" />
+                {/* <h3 className="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">로그인</h3>
+    <p className="mb-0">이메일과 비밀번호를 입력해주세요.</p> */}
+              </div>
+            </div>
+            <div className="flex-auto p-6">
+              <form onSubmit={handleSubmit(onLogin)}>
+                <label className="mb-2 ml-1 font-bold text-xs text-slate-700">User E-mail</label>
+                <div className="mb-4">
+                  <input
+                    {...register('empEmail')}
+                    placeholder="사용자 E-mail"
+                    type="text"
+                    className={classNames.input}
+                    aria-label="EmpEmail"
+                    aria-describedby="EmpEmail-addon"
+                  />
+                  <div className="text-xs text-red-600">{errors.empEmail?.message}</div>
+                </div>
+                <label className="mb-2 ml-1 font-bold text-xs text-slate-700">Password</label>
+                <div className="mb-4">
+                  <input
+                    {...register('empPassword')}
+                    placeholder="비밀번호"
+                    type="password"
+                    className={classNames.input}
+                    aria-label="Password"
+                    aria-describedby="Password-login"
+                  />
+                  <div className="text-xs text-red-600">{errors.empPassword?.message}</div>
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-cente text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
+                  >
+                    로그인
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="flex-auto p-6">
-            <form onSubmit={handleSubmit(onLogin)}>
-              <label className="mb-2 ml-1 font-bold text-xs text-slate-700">User E-mail</label>
-              <div className="mb-4">
-                <input
-                  {...register('empEmail')}
-                  placeholder="사용자 E-mail"
-                  type="text"
-                  className={classNames.input}
-                  aria-label="EmpEmail"
-                  aria-describedby="EmpEmail-addon"
-                />
-                <div className="text-xs text-red-600">{errors.empEmail?.message}</div>
-              </div>
-              <label className="mb-2 ml-1 font-bold text-xs text-slate-700">Password</label>
-              <div className="mb-4">
-                <input
-                  {...register('empPassword')}
-                  placeholder="비밀번호"
-                  type="password"
-                  className={classNames.input}
-                  aria-label="Password"
-                  aria-describedby="Password-login"
-                />
-                <div className="text-xs text-red-600">{errors.empPassword?.message}</div>
-              </div>
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-cente text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
-                >
-                  로그인
-                </button>
-              </div>
-            </form>
+          <div className="flex mt-10 justify-center">
+            <Image src={jnFirstLogo} className="inline w-16 h-full" alt="main_logo" />
+            <p className="font-bold text-xs text-center mt-[5px]">Copyright © J&First Co.,Ltd. All Rights Reserved</p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
