ab4c8866704e1d281b4a8ef46fd4e80b4b52c211
eunbbori <eunbbori@gmail.com>
2023-08-14 11:46:00 +0900

    FEATURE - 로그인 후 홈 표시의 의미가 불확실함
    ### DESC
    - 로고 화면 중앙에 띄우기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { Inter } from 'next/font/google';
+import jnFirstLogo from 'src/assets/img/logos/FAPIG.png';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex h-[calc(100vh-130px)] w-full justify-center items-center">
+      <Image src={jnFirstLogo} className="w-90 animate-pulse" alt="main_logo" />
+    </div>
+  );
 }
